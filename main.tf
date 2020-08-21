8eca00f208aef401f647104419768777a80a97a0
Dan Giffen <dangiffen@gmail.com>
2020-08-21 09:28:03 +0100

    Update TF backend

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "<YOUR-ORG-NAME>"
+    organization = "modular"
 
     workspaces {
       name = "learn-terraform-circleci"
